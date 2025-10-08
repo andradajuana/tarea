c437fc952a0f71aeec429b9432fa16222b13e1df
Andradajuana <andradajuana4@gmail.com>
2025-10-08 10:14:05 -0300

    commit erick

---
+++ trabajo-1.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Ingresa la primera nota: "))
+nota2 = float(input("Ingresa la segunda nota: "))
+promedio = (nota1 + nota2) / 2
+nombre = input("Ingresa el nombre del alumno: ")
+print(f"El promedio de {nombre} es {promedio}")
+notas_aprobadas = [6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
+aprobo = promedio in notas_aprobadas
+print(f"¿{nombre} aprobó la materia? {aprobo}")
