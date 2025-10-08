43a057bf5b6c10f4db7b73afe2f2f49a5c01d8df
Andradajuana <andradajuana4@gmail.com>
2025-10-08 10:17:22 -0300

    commit 2 erick

--- trabajo-1.py
+++ trabajo-1.py
@@ -6,3 +6,5 @@ print(f"El promedio de {nombre} es {promedio}")
 notas_aprobadas = [6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
 aprobo = promedio in notas_aprobadas
 print(f"¿{nombre} aprobó la materia? {aprobo}")
+
+nombre = "erick"
